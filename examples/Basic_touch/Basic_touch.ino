be604b0d62e7eb61d1f42120bad3c46935d5a20d
Bodmer <Bodmer@users.noreply.github.com>
2021-09-13 10:58:44 +0100

    Touch must be in range 1...N

--- examples/Basic_touch/Basic_touch.ino
+++ examples/Basic_touch/Basic_touch.ino
@@ -20,7 +20,7 @@ void setup() {
 void loop() {
   if(touch.pointDetected()) {
     uint8_t touches = touch.getPointCount();
-    for (int i = 0; i < touches; i++) {
+    for (int i = 1; i <= touches; i++) {
       Serial.print("Point "); Serial.print(0);
       Serial.print(": x = "); Serial.print(touch.getPointX[0]);
       Serial.print(", y = "); Serial.println(touch.getPointY[0]);
